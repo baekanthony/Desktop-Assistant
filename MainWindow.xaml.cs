d71a44ef252f3683c4f4d8799115ed6e2d77d158
Anthony Baek <baekanthony@gmail.com>
2024-07-01 10:02:48 -0700

    Azure CLU IOs working properly

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,6 +30,7 @@ namespace Assist
     public partial class MainWindow : Window
     {
         private SpeechSynthesizer synthesizer;
+        private ConversationAnalysisClient client;
         public MainWindow()
         {
             var builder = new ConfigurationBuilder()
@@ -38,11 +39,8 @@ namespace Assist
 
             Uri endpoint = new Uri(configuration["ApiSettings:Endpoint"]);
             AzureKeyCredential credential = new AzureKeyCredential($"{configuration["ApiSettings:Key"]}");
-            Console.WriteLine("endpoint: " + configuration["ApiSettings:Endpoint"]);
-            Console.WriteLine($"{configuration["ApiSettings:Key"]}");
 
-            ConversationAnalysisClient client = new ConversationAnalysisClient(endpoint, credential);
-            TestConnection(client);
+            client = new ConversationAnalysisClient(endpoint, credential);
             InitializeComponent();
             synthesizer = new SpeechSynthesizer();
             synthesizer.SetOutputToDefaultAudioDevice();
@@ -53,66 +51,84 @@ namespace Assist
         {
             if (e.Key == Key.Enter) 
             {
+                //TODO: just here temporarily to test synth working
                 synthesizer.Speak(txtInput.Text);
+
                 prevInput.Text += txtInput.Text + "\n";
-                ProcessCommand(txtInput.Text);
+                ProcessCommand(client, txtInput.Text);
                 txtInput.Clear();
             }
         }
 
-        //TODO: Connection working, start testing inputs and outputs
-        private void TestConnection(ConversationAnalysisClient client)
+        private void ProcessCommand(ConversationAnalysisClient client, String text)
         {
             string projectName = "Assistant";
             string deploymentName = "assistant-intent-v1";
 
             var data = new
             {
-                AnalysisInput = new
+                analysisInput = new
                 {
-                    ConversationItem = new
+                    conversationItem = new
                     {
-                        Text = "open firefox",
-                        Id = "1",
-                        ParticipantId = "1",
+                        text,
+                        id = "1",
+                        participantId = "1",
                     }
                 },
-                Parameters = new
+                parameters = new
                 {
-                    ProjectName = projectName,
-                    DeploymentName = deploymentName,
+                    projectName,
+                    deploymentName,
 
                     // Use Utf16CodeUnit for strings in .NET.
-                    StringIndexType = "Utf16CodeUnit",
+                    stringIndexType = "Utf16CodeUnit",
                 },
-                Kind = "Conversation",
+                kind = "Conversation",
             };
 
+            Console.WriteLine(RequestContent.Create(data));
             Response response = client.AnalyzeConversation(RequestContent.Create(data));
 
-            Console.WriteLine(response);
+            dynamic conversationalTaskResult = response.Content.ToDynamicFromJson(JsonPropertyNames.CamelCase);
+            dynamic conversationPrediction = conversationalTaskResult.Result.Prediction;
+
+            //TODO: try thinking about cases with combined commands (ex. open firefox, then turn on focus mode)
+
+            /*
+            Console.WriteLine($"Top intent: {conversationPrediction.TopIntent}");
+
+            Console.WriteLine("Entities:");
+            foreach (dynamic entity in conversationPrediction.Entities)
+            {
+                Console.WriteLine($"Category: {entity.Category}");
+                Console.WriteLine($"Text: {entity.Text}");
+                Console.WriteLine($"Confidence: {entity.ConfidenceScore}");
+                Console.WriteLine();
+
+            }
+            */
+
+            //TODO: maybe check confidenceScores here before proceeding
+            CommandSelection(conversationPrediction.TopIntent, conversationPrediction.Entities[0]);
         }
 
-        private String ProcessCommand(String text)
-        {
-            return "";
-        }
-
-        private void CommandSelection(String command, String target = "default")
+        private void CommandSelection(String command, dynamic target = null)
         {
             switch(command)
             {
-                case "open":
-                    Console.WriteLine("open");
+                case "Open":
+                    Console.WriteLine("Open");
+                    Console.WriteLine($"{target.Text}");
                     break;
-                case "search":
-                    Console.WriteLine("focus");
+                case "Focus":
+                    Console.WriteLine("Focus");
                     break;
-                case "focus":
-                    Console.WriteLine("swap");
+                case "Swap":
+                    Console.WriteLine("Swap");
                     break;
                 default:
-                    Console.WriteLine("invalid");
+                    Console.WriteLine("Invalid");
                     break;
             }
         }

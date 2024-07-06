f8d5560114a7b62aa781fa3962d0e41b030ac542
Anthony Baek <baekanthony@gmail.com>
2024-07-05 22:12:13 -0700

    Checking which monitors windows are being displayed on

--- HandleWindows.cs
+++ HandleWindows.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Collections.Generic;
 
 namespace Assist
 {
     internal class HandleWindows
     {
-        public HandleWindows() {
-            EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero, MonitorEnumProc, IntPtr.Zero);
-            EnumWindows(EnumWindowsTest, IntPtr.Zero);
-        }
-
         public struct RECT
         {
             public int Left;
@@ -18,17 +14,52 @@ namespace Assist
             public int Right;
             public int Bottom;
         }
+        public struct WINDOWINFO
+        {
+            public uint cbSize;
+            public RECT rcWindow;
+            public RECT rcClient;
+            public uint dwStyle;
+            public uint dwExStyle;
+            public uint dwWindowStatus;
+            public uint cxWindowBorders;
+            public uint cyWindowBorders;
+            public ushort atomWindowType;
+            public ushort wCreatorVersion;
+        }
+        public struct WINDOW
+        {
+            public RECT rect;
+            public IntPtr hWnd;
+
+            public WINDOW(RECT rect, IntPtr hWnd)
+            {
+                this.rect = rect;
+                this.hWnd = hWnd;
+            }
+        }
+        public const long WS_EX_NOREDIRECTIONBITMAP = 0x00200000L;
+        public const long WS_EX_TOOLWINDOW = 0x00000080L;
+
+        public List<RECT> monitors;
+        public List<WINDOW> windows;
+
+        public HandleWindows() {
+            monitors = new List<RECT>();
+            windows = new List<WINDOW>();
+            EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero, MonitorEnumProc, IntPtr.Zero);
+            EnumWindows(EnumWindowsTest, IntPtr.Zero);
+            SwapMonitors();
+        }
 
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lprcClip, EnumDisplayMonitorsProc lpfnEnum, IntPtr dwData);
         public delegate bool EnumDisplayMonitorsProc(IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData);
 
-        private static bool MonitorEnumProc(IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData)
+        private bool MonitorEnumProc(IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData)
         {
-            Console.WriteLine($"Monitor Handle: {hMonitor}");
-            Console.WriteLine($"Monitor Coordinates: Left = {lprcMonitor.Left}, Top = {lprcMonitor.Top}, Right = {lprcMonitor.Right}, Bottom = {lprcMonitor.Bottom}");
-            Console.WriteLine();
+            monitors.Add(lprcMonitor);
             return true;
         }
 
@@ -52,24 +83,7 @@ namespace Assist
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool GetWindowInfo(IntPtr hWnd, out WINDOWINFO pwi);
 
-        public struct WINDOWINFO
-        {
-            public uint cbSize;
-            public RECT rcWindow;
-            public RECT rcClient;
-            public uint dwStyle;
-            public uint dwExStyle;
-            public uint dwWindowStatus;
-            public uint cxWindowBorders;
-            public uint cyWindowBorders;
-            public ushort atomWindowType;
-            public ushort wCreatorVersion;
-        }
-
-        public const long WS_EX_NOREDIRECTIONBITMAP = 0x00200000L;
-        public const long WS_EX_TOOLWINDOW = 0x00000080L;
-
-        private static bool EnumWindowsTest(IntPtr hWnd, IntPtr lParam)
+        private bool EnumWindowsTest(IntPtr hWnd, IntPtr lParam)
         {
             if (IsWindowVisible(hWnd))
             {
@@ -88,16 +102,35 @@ namespace Assist
                     {
                         //If 2 windows have same coord, the one returned first is on top
                         //Design choice: How to handle minimize windows?
+                        WINDOW window = new WINDOW(rect, hWnd);
+                        windows.Add(window);
+                        /*
                         Console.WriteLine($"Window Handle: {hWnd}");
                         Console.WriteLine($"Window Title: {windowTitle}");
                         Console.WriteLine($"Window Info: rcWindow = {info.rcWindow}, rcClient = {info.rcClient}, dwStyle = {info.dwStyle.ToString("X")}, dwExStyle = {info.dwExStyle.ToString("X")}, dwWindowStatus = {info.dwWindowStatus}");
                         Console.WriteLine($"Window Rect: Left = {rect.Left}, Top = {rect.Top}, Right = {rect.Right}, Bottom = {rect.Bottom}");
                         Console.WriteLine();
+                        */
                     }
                 }
-
             }
             return true;
+        }
+
+        private void WindowOnMonitor(RECT windowCoords)
+        {
+            //windowCoords.Top < bot
+            //windowCoords.Bottom > top
+
+            //windowCoords.Right > left
+            //windowCoords.Left < right
+        }
+        private void SwapMonitors() 
+        {
+            //TODO: what if window is in both monitors? just leave it?
+            Console.WriteLine("printing");
+            Console.WriteLine(monitors.Count);
+            Console.WriteLine(windows.Count);
         }
     }
 }

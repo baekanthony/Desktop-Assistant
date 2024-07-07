97a6a43acd80b8d3de670c71004f4c290e97dc5d
Anthony Baek <baekanthony@gmail.com>
2024-07-06 20:06:08 -0700

    Testing out DwmGetWindowAttribute to see if I can get actual RECT values of windows

--- HandleWindows.cs
+++ HandleWindows.cs
@@ -59,6 +59,7 @@ namespace Assist
 
         private bool MonitorEnumProc(IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData)
         {
+            //Console.WriteLine($"monitor Rect: Left = {lprcMonitor.Left}, Top = {lprcMonitor.Top}, Right = {lprcMonitor.Right}, Bottom = {lprcMonitor.Bottom}");
             monitors.Add(lprcMonitor);
             return true;
         }
@@ -83,6 +84,30 @@ namespace Assist
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool GetWindowInfo(IntPtr hWnd, out WINDOWINFO pwi);
 
+
+        public enum DwmWindowAttribute
+        {
+            DWMWA_NCRENDERING_ENABLED = 1,
+            DWMWA_NCRENDERING_POLICY,
+            DWMWA_TRANSITIONS_FORCEDISABLED,
+            DWMWA_ALLOW_NCPAINT,
+            DWMWA_CAPTION_BUTTON_BOUNDS,
+            DWMWA_NONCLIENT_RTL_LAYOUT,
+            DWMWA_FORCE_ICONIC_REPRESENTATION,
+            DWMWA_FLIP3D_POLICY,
+            DWMWA_EXTENDED_FRAME_BOUNDS,
+            DWMWA_HAS_ICONIC_BITMAP,
+            DWMWA_DISALLOW_PEEK,
+            DWMWA_EXCLUDED_FROM_PEEK,
+            DWMWA_CLOAK,
+            DWMWA_CLOAKED,
+            DWMWA_FREEZE_REPRESENTATION,
+            DWMWA_LAST
+        };
+
+        [DllImport("dwmapi.dll")]
+        static extern int DwmGetWindowAttribute(IntPtr hWnd, DwmWindowAttribute dwAttribute, out RECT pvAttribute, int cbAttribute);
+
         private bool EnumWindowsTest(IntPtr hWnd, IntPtr lParam)
         {
             if (IsWindowVisible(hWnd))
@@ -94,6 +119,9 @@ namespace Assist
                 info.cbSize = (uint)Marshal.SizeOf(typeof(WINDOWINFO));
                 GetWindowInfo(hWnd, out info);
 
+                DwmWindowAttribute dwAttribute = DwmWindowAttribute.DWMWA_EXTENDED_FRAME_BOUNDS;
+                RECT dimensions;
+                DwmGetWindowAttribute(hWnd, dwAttribute, out dimensions, Marshal.SizeOf(typeof(RECT)));
                 //TODO: check for if the actual program itself is being filtered out
                 if (windowTitle.Length > 0 && ((info.dwExStyle & WS_EX_NOREDIRECTIONBITMAP) == 0) && ((info.dwExStyle & WS_EX_TOOLWINDOW) == 0))
                 {
@@ -104,6 +132,8 @@ namespace Assist
                         //Design choice: How to handle minimize windows?
                         WINDOW window = new WINDOW(rect, hWnd);
                         windows.Add(window);
+                        Console.WriteLine($"Window Title: {windowTitle}");
+                        Console.WriteLine($"Window Rect: Left = {dimensions.Left}, Top = {dimensions.Top}, Right = {dimensions.Right}, Bottom = {dimensions.Bottom}");
                         /*
                         Console.WriteLine($"Window Handle: {hWnd}");
                         Console.WriteLine($"Window Title: {windowTitle}");
@@ -117,17 +147,25 @@ namespace Assist
             return true;
         }
 
-        private void WindowOnMonitor(RECT windowCoords)
+        private bool IsWindowOnMonitor(RECT windowCoords, RECT monitorCoords)
         {
-            //windowCoords.Top < bot
-            //windowCoords.Bottom > top
-
-            //windowCoords.Right > left
-            //windowCoords.Left < right
+            return ((windowCoords.Left < monitorCoords.Right)
+                && (windowCoords.Top < monitorCoords.Bottom)
+                && (windowCoords.Right > monitorCoords.Left)
+                && (windowCoords.Bottom > monitorCoords.Top));
         }
+
+        //Mathematical way to determine the border?
         private void SwapMonitors() 
         {
             //TODO: what if window is in both monitors? just leave it?
+            foreach (WINDOW window in windows)
+            {
+                foreach (RECT monitor in monitors)
+                {
+                    IsWindowOnMonitor(window.rect, monitor);
+                }
+            }
             Console.WriteLine("printing");
             Console.WriteLine(monitors.Count);
             Console.WriteLine(windows.Count);
